2be92f9b61e86c5441dae5df14b779a76fbc9a98
di3goCS <diegosilva282@hotmail.com>
2019-10-19 12:43:30 -0300

    Migrations de Instituicao e Redes Sociais

--- nasa-app/database/migrations/2019_10_19_152002_create_instituicao_table.php
+++ nasa-app/database/migrations/2019_10_19_152002_create_instituicao_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateInstituicaoTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('instituicao', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('razao_social', 150);
+            $table->string('cnpj', 11);
+            $table->string('objetivo', 255);
+            $table->string('area_atuacao', 255);
+            $table->string('email', 100);
+            $table->string('telefone', 15);
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('instituicao');
     }
 }
